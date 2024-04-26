2da2001f2183710e00f503f922526a3e4f2662c1
Julio Cesar <julio.pereira.t0126213@edu.atec.pt>
2024-04-26 15:57:43 +0100

    This reverts commit be4965bdd7b7a661ce6aef63d4963467732cd9ad.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-Component({
+@Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
