61d1960a099866d596e83489d0019dbb91428ad5
Julio Cesar <julio.pereira.t0126213@edu.atec.pt>
2024-05-14 22:19:10 +0100

    feat: componente header com estrutura inicial e scss centralizado, introduzindo o styles/

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { __decorate } from "tslib";
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+
 }
