a6c79705c76fc6ffcd1702eede1225b9b5433c67
Julio Cesar <julio.pereira.t0126213@edu.atec.pt>
2024-05-17 23:29:27 +0100

    feat: angular modularizado -front

---
+++ frontend/src/app/components/data/data-inputs/data-inputs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-inputs',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-inputs.component.html',
+  styleUrl: './data-inputs.component.scss'
+})
+export class DataInputsComponent {
+
+}
