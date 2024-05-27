3e7197993467888954ffb061972c22c5a52274be
Júlio César Pereira <jc.imp10@gmail.com>
2024-05-27 19:07:45 +0100

    fix: deploy to render final 12

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+# Iniciar PHP-FPM
 php-fpm -D
 
 # Aguarde o banco de dados estar disponível
 dockerize -wait tcp://dpg-cp50v0f79t8c73emtbjg-a.frankfurt-postgres.render.com:5432 -timeout 60s
 
-# Verifique se a variável RESET_SEEDERS está definida como "true"
+# Execute as migrações e seeds conforme necessário
 if [ "$RESET_SEEDERS" = "true" ]; then
-    echo "Resetting seeders..."
     php artisan migrate:fresh --seed --force
 else
-    echo "Running migrations without seeding..."
     php artisan migrate --force
 fi
 
 # Ajuste as sequências
 php artisan db:adjust-sequences
 
-# Inicie o Nginx
+# Inicie o Nginx em primeiro plano
 nginx -g "daemon off;"
