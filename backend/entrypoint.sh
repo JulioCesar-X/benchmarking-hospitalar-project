d6cd70316fde1f91fd264dee84fdd8fcb339bc80
Júlio César Pereira <jc.imp10@gmail.com>
2024-05-28 12:28:25 +0100

    fix: deploy to render final 28

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Espera pelo banco de dados estar disponível
 dockerize -wait tcp://dpg-cp50v0f79t8c73emtbjg-a.frankfurt-postgres.render.com:5432 -timeout 60s
 
+# Executa migrações com ou sem seeders
 if [ "$RESET_SEEDERS" = "true" ]; then
     php artisan migrate:fresh --seed --force
 else
     php artisan migrate --force
 fi
 
+# Ajusta as sequências no banco de dados
 php artisan db:adjust-sequences
 
-exec "$@"
+# Inicia o PHP-FPM
+exec php-fpm -F
