127915402e303fe5de3430bb30e1f01cbf44cbe0
João Silva <61765922+CarDioLogic@users.noreply.github.com>
2024-06-08 13:06:15 +0100

    added tailwind framework

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,7 +6,7 @@
 #dockerize -wait tcp://db:5432 -timeout 60s
 
 # #comente esse para o ambiente de desenvolvimento
-# dockerize -wait tcp://dpg-cp50v0f79t8c73emtbjg-a:5432 -timeout 60s
+ dockerize -wait tcp://dpg-cp50v0f79t8c73emtbjg-a:5432 -timeout 60s
 
 php artisan clear:all
 
