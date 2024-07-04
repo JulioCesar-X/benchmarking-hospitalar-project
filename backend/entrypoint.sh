921ea24e5b1f7c64bf2376dad3c90b61a6b5b787
Júlio César Pereira <jc.imp10@gmail.com>
2024-07-04 12:28:51 +0100

    deploy8

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,17 +3,27 @@
 # Wait for the database to be ready
 dockerize -wait tcp://$DB_HOST:$DB_PORT -timeout 60s
 
+# Start the Laravel application in the background
+php artisan serve --host=0.0.0.0 --port=8000 &
+
+echo "Laravel application started."
+
+# Clear caches
 php artisan clear:all
 
-# Run migrations
+echo "Running migrations and seeders in the background."
+
+# Run migrations and seeders in the background
 if [ "$RESET_SEEDERS" = "true" ]; then
-    php artisan migrate:fresh --seed
+    php artisan migrate:fresh --seed &
 else
-    php artisan migrate --force
+    php artisan migrate --force &
 fi
 
-# Ajustar sequências do banco de dados se necessário
-php artisan db:adjust-sequences
+# Ajustar sequências do banco de dados se necessário em background
+php artisan db:adjust-sequences &
 
-# Start the Laravel application
-php artisan serve --host=0.0.0.0 --port=8000
+# Wait for background processes to finish
+wait
+
+echo "Migrations and seeders completed."
